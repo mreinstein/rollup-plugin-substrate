dd8c57832d7eb7708568fa80933c66484e3f46d8
Mike Reinstein <mreinstein@sellpoints.com>
2020-10-26 12:26:57 -0700

    update deps to support es9

--- plugin.cjs.js
+++ plugin.cjs.js
@@ -15429,8 +15429,7 @@ function buildModule (src) {
                 return
 
             try {
-                const program = espree_1.parse(token.text, { ecmaVersion: 6, sourceType: 'module' });
-
+                const program = espree_1.parse(token.text, { ecmaVersion: 9, sourceType: 'module' }); 
                 const isExplorable =(langParts[1] === 'explorable');
 
                 if (!isExplorable)
